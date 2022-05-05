8952f761b853d738312b9f009941720faa2d0027
Austin0914 <87524840+Austin0914@users.noreply.github.com>
2022-05-05 21:21:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 from discord.ext import commands
 
 bot=commands.Bot(command_prefix="!")
-TOKEN='_______'
+TOKEN='_________'
 
 @bot.command()
 async def feature(ctx):
@@ -13,11 +13,18 @@ async def feature(ctx):
         url="https://github.com/Austin0914/Discord-Bot",
         color=discord.Color.blue())
     feature.set_author(name="Austin0914", url="https://github.com/Austin0914", icon_url="https://avatars.githubusercontent.com/u/87524840?v=4")
-    feature.add_field(name="!move", value="`NAME` `Channel_A` `Channel_B` `Time`", inline=False)
+    feature.add_field(name="!move", value="!move `NAME` `Channel_A` `Channel_B` `Time`(s)", inline=False)
+    feature.add_field(name="!Austin", value="Colorful Egg", inline=False)
     await ctx.send(embed=feature)
 
 @bot.command()
+async def Austin(ctx):
+    for i in range(0,10,1):
+        await ctx.send("Austin is Handsome!!")
+
+@bot.command()
 async def move( ctx , member:discord.Member ,channel: discord.VoiceChannel,channel_2:discord.VoiceChannel,time):
+    await ctx.send("LOL")
     for i in range(0,int(time),1):
         await asyncio.sleep(0.5)
         if(i%2==0):
